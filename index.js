015ed31a5ac33521c0640ea05d7e4f853ee320a3
Kat <kyevsukov@gmail.com>
2021-10-28 12:48:45 -0400

    added ports

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Web 47 rocks!</h1>
+  `)
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
+
 
 function cohortRocks() {
     console.log('arguments:', process.argv.slice(2))
